74c779a7430e14559fde5f2dc8faad07162d1eec
joaolennons <joaolennons@gmail.com>
2018-10-22 11:17:10 -0300

    add onesignal structure

---
+++ src/Cow.io.OneSignal/OneSignalPushDispatcher.cs
@@ -0,0 +1,17 @@
+﻿using Cow.io.PushNotification;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Cow.io.OneSignal
+{
+    internal class OneSignalPushDispatcher : IPushNotificationDispatcher
+    {
+        public Task<IPushNotificationResponse> Dispatch(IPushNotification notification, CancellationToken cancellation = default(CancellationToken))
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
